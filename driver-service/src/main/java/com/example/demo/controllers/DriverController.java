a6a333c72242d327de40d8524f9d29ca5a6a9747
Manoj Manwadkar <manojmanwadkar@gmail.com>
2022-01-22 17:08:11 +0530

    First

--- driver-service/src/main/java/com/example/demo/controllers/DriverController.java
+++ driver-service/src/main/java/com/example/demo/controllers/DriverController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +9,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = "api/v1")
 public class DriverController {
 	
+	@Value("${server.port}")
+	private int port;
+	
+	@Value("${author.location}")
+	private String auther;
+	
 	@GetMapping(path = "/drivers")
 	public String getDrivers() {
-		return "Vishal,Vicky,Ramesh,Suresh,Mangesh";
+		return auther+" Added:" + "Vishal,Vicky,Ramesh,Suresh,Mangesh"+ port;
 	}
 	
 
